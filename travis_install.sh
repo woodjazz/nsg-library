59a09688dbc8926d6fc1076b9e9e6e5bee0def38
Néstor Silveira Gorski <silveira.nestor@gmail.com>
2015-03-16 11:05:40 +0100

    Update travis_install.sh

--- travis_install.sh
+++ travis_install.sh
@@ -3,12 +3,12 @@ set -e
 case "${TRAVIS_OS_NAME}" in
   linux)
     # INSTALL gcc 4.8.1 AND g++ 4.8.1
-    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test\
-    sudo apt-get update -qq\
-    sudo apt-get install -qq gcc-4.8 g++-4.8\
-    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90\
+    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test \
+    sudo apt-get update -qq \
+    sudo apt-get install -qq gcc-4.8 g++-4.8 \
+    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90 \
     # GET LINUX BUILD DEPENDENCIES
-    sudo apt-get install -y libglu1-mesa-dev\
+    sudo apt-get install -y libglu1-mesa-dev \
     sudo apt-get install -q -y --no-install-recommends libasound2-dev
     ;;
   osx)
