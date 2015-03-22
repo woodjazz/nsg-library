a0118ed9a9bc0922a3a8db0aca57dc44d980d68d
Néstor Silveira <silveira.nestor@gmail.com>
2015-03-22 12:46:13 +0100

    ...

--- travis_script.sh
+++ travis_script.sh
@@ -2,7 +2,9 @@
 set -e
 case "${TRAVIS_OS_NAME}" in
   linux)
-  	./cmake_linux.sh linuxbuild && cd ../linuxbuild && make && make test
+	mkdir debug && cd debug && cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE="Debug" && make && make test
+	cd ..
+	mkdir release && cd release && cmake .. -G "Unix Makefiles" && make && make test
     ;;
   osx)
 	./cmake_osx.sh osxbuild && cd ../osxbuild && xcodebuild -target ALL_BUILD && xcodebuild -target RUN_TESTS
