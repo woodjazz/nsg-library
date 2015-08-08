5d3c64476d60f9ceb8799027d35e13c90e815f43
Néstor Silveira <silveira.nestor@gmail.com>
2015-08-08 18:09:39 +0200

    ...

--- NSG/animation/Animation.cpp
+++ NSG/animation/Animation.cpp
@@ -36,7 +36,7 @@ misrepresented as being the original software.
 
 namespace NSG
 {
-	template<> std::map<std::string, PAnimation> StrongFactory<std::string, Animation>::objsMap_ = {};
+	template<> std::map<std::string, PAnimation> StrongFactory<std::string, Animation>::objsMap_ = std::map<std::string, PAnimation>{};
 
     AnimationKeyFrame::AnimationKeyFrame()
         : time_(0),
