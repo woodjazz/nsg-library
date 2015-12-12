bbc8d60f8c49411f6a5ae9ac0332cb3ce8e840b3
Néstor <nestor@IMAC5K.local>
2015-12-12 19:23:48 +0100

    ...

--- travis_install.sh
+++ travis_install.sh
@@ -10,8 +10,11 @@ case "${TRAVIS_OS_NAME}" in
     # sudo apt-get install -qq gcc-4.8 g++-4.8
     # sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
     # GET LINUX BUILD DEPENDENCIES
-    sudo apt-get install -y libglu1-mesa-dev
+    # sudo apt-get install -y libglu1-mesa-dev
     # sudo apt-get install -q -y --no-install-recommends libasound2-dev
+    sudo apt-get install xorg-dev
+    sudo apt-get install libglu1-mesa-dev
+    sudo apt-get install libasound2-dev
     ;;
   osx)
     brew install Caskroom/cask/xquartz
